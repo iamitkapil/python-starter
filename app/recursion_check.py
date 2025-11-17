cd25ed155f5b4a46ab617632432f95f0cc5062e6
Amit Kapil <amit.kapil@partner.bmw.de>
2025-11-17 10:14:55 +0530

    update recursion code

--- app/recursion_check.py
+++ app/recursion_check.py
@@ -1,7 +1,18 @@
-def solve(x):
-    if x <= 1:
-        return x
+def solve(x, depth=0):
+    if x <= 0:
+        return 1
+    if depth > 5:
+        return x + solve(x - 1, depth - 1)
     if x % 2 == 0:
-        return solve(x - 1) + solve(x - 2) + solve(x - 3)
+        return (
+            solve(x - 1, depth + 1) + 
+            solve(x - 2, depth + 1) +
+            solve(x - 3, depth + 2) +
+            solve(x - 4, depth + 3)
+        )
     else:
-        return solve(x - 1) * 2 + solve(x - 3)
+        return (
+            solve(x - 1, depth + 2) * 2 - 
+            solve(x - 3, depth + 1) +
+            solve(x - 5, depth + 4)
+        ) + solve(x // 2, depth + 3)

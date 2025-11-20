ac12968c6a8839b38311b0e38268d9424eb6fd67
Amit Kapil <amit.kapil@partner.bmw.de>
2025-11-20 11:04:43 +0530

    commented  code to check for test coverage

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,18 +1,18 @@
-from app.calculator import add, subtract, multiply, divide
-import pytest
+# from app.calculator import add, subtract, multiply, divide
+# import pytest
 
-def test_add():
-    assert add(2, 5) == 7
+# def test_add():
+#     assert add(2, 5) == 7
 
-def test_subtract():
-    assert subtract(10, 3) == 7
+# def test_subtract():
+#     assert subtract(10, 3) == 7
 
-def test_multiply():
-    assert multiply(4, 5) == 20
+# def test_multiply():
+#     assert multiply(4, 5) == 20
 
-def test_divide():
-    assert divide(20, 5) == 4
+# def test_divide():
+#     assert divide(20, 5) == 4
 
-def test_divide_by_zero():
-    with pytest.raises(ValueError):
-        divide(10, 0)
+# def test_divide_by_zero():
+#     with pytest.raises(ValueError):
+#         divide(10, 0)
